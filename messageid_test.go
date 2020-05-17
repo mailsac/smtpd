f99c54f79168122f6209dc24374d74f7b57e6f59
Jeff Parrish <jeffhparrish@gmail.com>
2020-05-17 09:05:09 -0700

    Message ID randomnesss was not sufficient
    1. Moves the message ID generation to its own file.
    2. Under heavy load, if crypto random source is exhausted, a psuedo-rand
    source is used. But the pseudo-random source was never initialized
    with a new seed, so it would be using the same seed across all inbound
    instances. This will use the unix nano time now, which should be different
    even if the instances were deployed at almost the same time.
    3. Rather than replace the non-alphanumeric chars in the base64 version
    of the message ID with predetermined ones, use a pseudo-random character.
    4. Allow underscore in ids as one more piece of randomness.

--- messageid_test.go
+++ messageid_test.go
@@ -1,19 +1,21 @@
 package smtpd_test
 
 import (
+	"fmt"
 	"github.com/ruffrey/smtpd"
-	"math/rand"
 	"testing"
-	"time"
 )
 
 func Test_MessageID(t *testing.T) {
 	t.Run("NewMessageID is unlikely to collide", func(t *testing.T) {
-		rand.Seed(time.Now().UnixNano())
+		smtpd.InitPseudoRandomNumberGeneratorFallback()
 		o := make(map[string]bool)
 		var id string
 		for i := 0; i < 1000000; i++ {
 			id = smtpd.NewMessageID()
+			if i % 500000 == 0 {
+				fmt.Println("NewMessageID test: ", id)
+			}
 			if o[id] {
 				t.Errorf("Got duplicate unique id %d (%s)", i, id)
 				t.FailNow()
