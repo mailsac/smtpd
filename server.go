693a7653f3c3e808e2eb77fdd4f6b4bc4b829c5b
Michael Mayer <mjmayer@gmail.com>
2022-07-07 07:41:58 -0700

    Put TLS log message behind verbose flag

--- server.go
+++ server.go
@@ -502,8 +502,9 @@ ReadLoop:
 			tlsConn.SetDeadline(time.Now().Add(s.WriteTimeout))
 			if err := tlsConn.Handshake(); err == nil {
 				newID := NewMessageID()
-				// TODO Add logging of upgrade to TLS when log levels are implemented
-				// s.Logger.Printf("Upgraded TLS %v to %v", conn.ID, newID)
+				if conn.server.Verbose {
+					s.Logger.Printf("Upgraded TLS. Changed messageId from %v to %v", conn.ID, newID)
+				}
 				conn = &Conn{
 					ID:                newID,
 					Conn:              tlsConn,
